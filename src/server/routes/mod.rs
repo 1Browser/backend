b744c4ea2516f7615adeffda9afb0bac5f865d24
KallyDev <kallydev@rss3.io>
2024-09-22 19:21:18 +0800

    fix: invalid path for cors middleware

--- src/server/routes/mod.rs
+++ src/server/routes/mod.rs
@@ -1,6 +1,7 @@
 use ::oauth2::basic::BasicClient;
 use axum::{Extension, Router};
 use sqlx::PgPool;
+use tower_http::cors::CorsLayer;
 
 mod oauth2;
 mod user;
@@ -9,5 +10,6 @@ pub fn new(pg_pool: PgPool, oauth2_client: BasicClient) -> Router {
     Router::new()
         .nest("/oauth2", oauth2::new_router(oauth2_client))
         .nest("/users", user::new_router())
+        .layer(CorsLayer::permissive())
         .layer(Extension(pg_pool))
 }
--- src/server/routes/user.rs
+++ src/server/routes/user.rs
@@ -2,12 +2,10 @@ use crate::database::models::user::User;
 use crate::server::middlewares::auth::authorize;
 use axum::routing::get;
 use axum::{middleware, Extension, Json, Router};
-use tower_http::cors::CorsLayer;
 
 pub fn new_router() -> Router {
     Router::new()
         .route("/@me", get(get_me))
-        .layer(CorsLayer::permissive())
         .layer(middleware::from_fn(authorize))
 }
 
